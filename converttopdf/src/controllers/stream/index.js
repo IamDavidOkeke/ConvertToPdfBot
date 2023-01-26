203e1183b7bf1ed84b9179d5ef6ac3493e32d5ab
David Okeke <iamdavonokeke@gmail.com>
2023-01-27 00:25:45 +0100

    adding stream rule

--- converttopdf/src/controllers/stream/index.js
+++ converttopdf/src/controllers/stream/index.js
@@ -1,12 +1,19 @@
 var appClient = require('../../appClient')
 
-
-var stream = (async ()=>{
+var updateStreamRules = async ()=>{
     await appClient.v2.updateStreamRules({
         add:[
-            { value: '@save_pdf', tag: 'save_pdf'}
+            { value: '("@save_pdf save") -is:retweet', tag: 'save_pdf'}
         ]
       })
+}
+var getStreamRules = async ()=>{
+    var rules= await appClient.v2.streamRules()
+    return rules
+}
+var stream = (async ()=>{
+    await updateStreamRules()
+    console.log(await getStreamRules())
     var streams = await appClient.v2.searchStream()
     return streams
 })
