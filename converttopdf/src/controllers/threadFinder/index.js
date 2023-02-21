dbfc92553f6c35434a1db0ea6de7570d12d1cd6d
David Okeke <iamdavonokeke@gmail.com>
2023-02-21 13:52:59 +0100

    adding media to tweets from includes object

--- converttopdf/src/controllers/threadFinder/index.js
+++ converttopdf/src/controllers/threadFinder/index.js
@@ -2,7 +2,7 @@ var appClient = require('../../appClient')
 
 var params = {
     'tweet.fields':['author_id','conversation_id', 'attachments'],
-    'media.fields':['media_key', 'type','url', 'alt_text'],
+    'media.fields':['media_key', 'type','url', 'alt_text', 'preview_image_url'],
     expansions: ['attachments.media_keys']
 }
 
@@ -17,30 +17,37 @@ var getReplies = async function(conversationId, authorId){
     return replies
 }
 var getAllTweets = async function(threadPaginator){
-    var threadPaginatorAll
+    var threadPaginatorAll = threadPaginator
     while(!threadPaginator.done){
         threadPaginatorAll = await threadPaginator.fetchNext()
     }
     return threadPaginatorAll
 }
-var aggregatedThread = function(paginator, arr){
+var aggregatedThread = function(paginator){
+    var arr = []
     for (const fetchedTweet of paginator){
+        fetchedTweet.media = paginator.includes.medias(fetchedTweet)
         console.log('fetchedTweet',fetchedTweet)
         arr.unshift(fetchedTweet)
         }
     return arr
-}
+} 
 var getThread =  async function (conversationId){
-    var thread = []
-    var originalTweet = await findOriginalTweet(conversationId)
-    thread.unshift(originalTweet)
-    var authorId = originalTweet.data.author_id
-    //console.log ('Twitter has sent something about author:', originalTweet)
-    var threadPaginator = await getReplies(conversationId, authorId)
-    //console.log('threadPaginator', threadPaginator)
-    var threadPaginatorAll = await getAllTweets(threadPaginator)
-    //console.log('threadPaginatorAll2', threadPaginatorAll)
-    return  aggregatedThread(threadPaginatorAll, thread)
+    try{
+        var originalTweet = await findOriginalTweet(conversationId)
+        var authorId = originalTweet.data.author_id
+        console.log ('Twitter has sent something about author:', originalTweet )
+        var threadPaginator = await getReplies(conversationId, authorId)
+        console.log('threadPaginator', threadPaginator)
+        var threadPaginatorAll = await getAllTweets(threadPaginator)
+        console.log('threadPaginatorAll2', threadPaginatorAll)
+        var thread = aggregatedThread(threadPaginatorAll)
+        thread.unshift(originalTweet)
+        return thread
+    }catch(e){
+        console.log(e)
+    }
+    
 }
 
 module.exports = { getThread }
