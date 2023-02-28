694f7289db36a7b78fbd6c252083b009a2dbbf62
David Okeke <iamdavonokeke@gmail.com>
2023-02-28 20:32:15 +0100

    adding pdf document route

--- converttopdf/routes/pdf.js
+++ converttopdf/routes/pdf.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var { pdfHandler } = require('../src/controllers/pdfHandler')
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
   res.render('pdf', { title: 'A pdf view' , id: req.params.id});
 });
+router.get('/:id/pdf', function(req, res, next){
+  pdfHandler(req, res, next)
+} );
 
 module.exports = router;
