7f0052a524191613816fb441c3ff94f7758659df
David Okeke <iamdavonokeke@gmail.com>
2023-03-03 09:53:26 +0100

    adding main.js file

---
+++ converttopdf/src/main.js
@@ -0,0 +1,27 @@
+var stream = require('./controllers/stream');
+var { ETwitterStreamEvent } = require('twitter-api-v2')
+var appClient = require('./appClient')
+var twitterClient = require('./twitterClient.js')
+var { verifyReply, verifyMention, getReply } = require('./controllers/sdk');
+
+var main = async function(){
+    var tweetStream = await stream(appClient)
+
+    tweetStream.on(
+        ETwitterStreamEvent.Data,
+         async (tweet) => {
+          console.log('Twitter has sent something:', tweet.data.entities)
+          if (verifyMention(tweet) && verifyReply(tweet)){
+            let id = tweet.data.id
+            let conversation_id = tweet.data.conversation_id
+            let reply = getReply(conversation_id)
+            twitterClient.v2.reply(reply , id)
+          }
+          else {
+            console.log('not verified')
+          }
+        }
+      );
+}
+
+module.exports = main
