1da61663a9bb620cfea4fd0ec02d53a4d5610070
David Okeke <iamdavonokeke@gmail.com>
2023-03-03 09:55:28 +0100

    adding main function

--- converttopdf/app.js
+++ converttopdf/app.js
@@ -5,10 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var pdfViewRouter = require('./routes/pdf');
+var main = require('./src/main')
 var app = express();
-
+main()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -20,13 +20,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/pdf', pdfViewRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
