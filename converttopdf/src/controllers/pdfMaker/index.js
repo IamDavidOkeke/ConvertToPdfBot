b3bc5685cd1d8d7193aa60a122d307b25b3313c0
David Okeke <iamdavonokeke@gmail.com>
2023-03-03 09:37:43 +0100

    adding media and pdf options paramenters

--- converttopdf/src/controllers/pdfMaker/index.js
+++ converttopdf/src/controllers/pdfMaker/index.js
@@ -2,9 +2,20 @@ var fs = require('fs')
 var PDFDocument = require('pdfkit')
 var path = require('path')
 var {convertUrlToFile} = require('./urlToFile')
+var {thready} = require('./thread')
 
 var options = {
-    autoFirstPage:false
+    autoFirstPage:false,
+    size: [ 580, 420 ],
+    margins: {
+        top:48,
+        bottom:48,
+        left:48,
+        right:48
+    }
+}
+var imageOptions = {
+    width:150
 }
 var mediaPath = path.join(process.cwd(), '/public/pdf/media')
 
@@ -21,26 +32,22 @@ var removeMedia = function(path){
 
 var addMedias = async function(medias){
     for(let i = 0; i < medias.length; i++){
-        if(medias[i].type === 'photo'){
-            let imagePath = await convertUrlToFile(medias[i].url, mediaPath) 
-            doc.image(imagePath)
-            removeMedia(imagePath)
-        }else {
-            let imagePath = await convertUrlToFile(medias[i].preview_image_url, mediaPath) 
-            doc.image(imagePath)
-            removeMedia(imagePath)
-        }
+        let url = medias[i].type === 'photo'? medias[i].url : medias[i].preview_image_url
+        let imagePath = await convertUrlToFile(url, mediaPath) 
+        doc.image(imagePath,undefined, undefined, {...imageOptions, link:url})
+        removeMedia(imagePath)
     }
 }
 
 
-var makePdf = async function(writeStream, thread){
+var makePdf = async function(writeStream, thread=thready){
     try{
         doc.pipe(writeStream)
             for(let i = 0; i < thread.length; i++){
                 let text = thread[i].text || thread[i].data.text
                 let medias = thread[i].media || thread[i].includes?.media 
                 doc.addPage()
+                doc.font('font/PTSans-Regular.ttf')
                 doc.text(text)
                 if(medias && medias.length){
                    await addMedias(medias)
