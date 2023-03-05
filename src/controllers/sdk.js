0f18c6eacb4da22f2a48832130f8ec2f0b0c0d37
David Okeke <iamdavonokeke@gmail.com>
2023-03-05 19:38:07 +0100

    adding environmental url variable

--- src/controllers/sdk.js
+++ src/controllers/sdk.js
@@ -33,7 +33,7 @@ var verifyMention = function(tweet){
   }
 
   var getReply = function(id){
-    return `Hello, \nHere is your pdf \nhttps://savetweetpdf.web.app/pdf/${id}`
+    return `Hello, \nHere is your pdf \n${process.env.CYCLIC_URL}/pdf/${id}`
   }
   
 module.exports = {verifyReply, verifyMention, getReply}
