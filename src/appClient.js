19386fd9b56f2033bf9be5f0fea5b8de30f1d779
David Okeke <iamdavonokeke@gmail.com>
2023-03-05 19:36:42 +0100

    adding environmental bearer token

--- src/appClient.js
+++ src/appClient.js
@@ -1,6 +1,5 @@
-const  credentials   = require('../credentials')
 const { TwitterApi } =  require('twitter-api-v2')
 
-const  appClient  =  new TwitterApi(credentials.bearerToken)
+const  appClient  =  new TwitterApi(process.env.BEARER_TOKEN)
 
 module.exports = appClient
