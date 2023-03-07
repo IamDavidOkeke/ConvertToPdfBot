9d0604d9af6bbb5a83917b91eeab8895d609d233
David Okeke <iamdavonokeke@gmail.com>
2023-03-07 09:15:07 +0100

    set response type to pdf

--- src/controllers/pdfHandler/index.js
+++ src/controllers/pdfHandler/index.js
@@ -4,6 +4,7 @@ const { getThread } = require("../threadFinder")
 
 var pdfHandler = async function(req, res, next){
     try{
+        res.type( "pdf" )
         let id = req.params.id
         let thread = await getThread(id)
         makePdf(res, thread)
