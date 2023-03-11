62c91b2fe9119d6b8fe634280329fc45734c6375
David Okeke <iamdavonokeke@gmail.com>
2023-03-11 18:54:27 +0100

    reverting to pipe method

--- routes/pdf.js
+++ routes/pdf.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var { pdfHandler } = require('../src/controllers/pdfHandler')
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res) {
   res.render('pdf', { title: 'A pdf view' , id: req.params.id});
 });
-router.get('/:id/pdf', function(req, res, next){
-  pdfHandler(req, res, next)
+router.get('/:id/pdf', async function(req, res, next){
+  await pdfHandler(req, res)
 } );
 
 module.exports = router;
--- src/controllers/pdfMaker/index.js
+++ src/controllers/pdfMaker/index.js
@@ -19,23 +19,19 @@ var doc = new PDFDocument(options)
 
 var makePdf = async function(writeStream, thread){
     try{
-        var buffers = [];
-        doc.on( "data", function( chunk ) { buffers.push( chunk ); } );
-            for(let i = 0; i < thread.length; i++){
-                let text = thread[i].text || thread[i].data.text
-                let medias = thread[i].media || thread[i].includes?.media 
-                doc.addPage()
-                doc.font('font/PTSans-Regular.ttf')
-                doc.text(text)
-                if(medias && medias.length){
-                   doc.moveDown()
-                   await addMedias(medias, doc)
-                }
+        doc.pipe(writeStream)
+        for(let i = 0; i < thread.length; i++){
+            let text = thread[i].text || thread[i].data.text
+            let medias = thread[i].media || thread[i].includes?.media 
+            doc.addPage()
+            doc.font('font/PTSans-Regular.ttf')
+            doc.text(text)
+            if(medias && medias.length){
+                doc.moveDown()
+                await addMedias(medias, doc)
+            }
             }
         doc.end()
-        doc.on( "end", function() {
-            writeStream.end( Buffer.concat( buffers ), "binary" );
-        } );
     }catch(e){
         console.log(e)
     }
