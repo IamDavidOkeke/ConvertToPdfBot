201b963ce7d482f2436ab3439b958628afeb29be
David Okeke <iamdavonokeke@gmail.com>
2023-03-13 09:37:24 +0100

    adding username and profile photo to pdf

--- src/controllers/pdfMaker/index.js
+++ src/controllers/pdfMaker/index.js
@@ -1,14 +1,26 @@
 var {addMedias} = require('./mediaHandler')
+const { convertUrlToBuffer } = require('./urlToBuffer')
 
 
 var makePdf = async function(doc, thread){
     try{
+        let OriginalTweet = thread[0]
+        let {username, name, profile_image_url } = OriginalTweet.includes.users[0]
+        let profileImageBuffer = await convertUrlToBuffer(profile_image_url)
+
         for(let i = 0; i < thread.length; i++){
             let text = thread[i].text || thread[i].data.text
             let medias = thread[i].media || thread[i].includes?.media 
             doc.addPage()
-            doc.font('font/PTSans-Regular.ttf')
-            doc.text(text)
+               .image(profileImageBuffer, undefined, undefined,{width:60}) 
+               .moveDown(0.2)
+               .fillColor('#555555')
+               .text(name)
+               .text(`@${username}`)
+               .moveDown(0.5)
+               .fillColor('black')
+               .font('font/PTSans-Regular.ttf')
+               .text(text)
             if(medias && medias.length){
                 doc.moveDown()
                 await addMedias(medias, doc)
